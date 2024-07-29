ac4cf07620b515c745d0b70c8ff255138c9cf3f8
nishaj3 <93059833+nishaj3@users.noreply.github.com>
2024-07-29 10:18:31 -0400

    hkbkh

--- scripts/chartScript.js
+++ scripts/chartScript.js
@@ -1,78 +1,63 @@
+d3.csv('data/gender-wage-gap-vs-gdp-per-capita.csv').then(function(data) {
+  // Filter data for the year 2015
+  const filteredData = data.filter(d => d.Year === '2015');
 
-// Add a tooltip div to display information
-const tooltip = d3.select("body").append("div")
-  .attr("class", "tooltip")
-  .style("position", "absolute")
-  .style("background-color", "#fff")
-  .style("border", "1px solid #ddd")
-  .style("border-radius", "4px")
-  .style("padding", "10px")
-  .style("visibility", "hidden");
-
-// Load the data
-d3.csv("data/gender-wage-gap-vs-gdp-per-capita.csv").then(function(data) {
-  // Data processing
-  data.forEach(d => {
-    d[' gender_wage_gap'] = +d[' gender_wage_gap'] || 0;
-    d[' GDP_per_capita'] = +d[' GDP_per_capita'] || 0;
-  });
-
-  // Define the margins and dimensions
-  const margin = { top: 20, right: 30, bottom: 40, left: 40 };
+  const margin = { top: 20, right: 30, bottom: 40, left: 50 };
   const width = 960 - margin.left - margin.right;
   const height = 500 - margin.top - margin.bottom;
 
-  // Create SVG
-  const svg = d3.select("#plot3")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+  const svg = d3.select('#plot3')
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
 
-  // Define scales
   const x = d3.scaleLinear()
-    .domain(d3.extent(data, d => d[' GDP_per_capita'])).nice()
-    .range([0, width]);
+      .domain(d3.extent(filteredData, d => +d[' GDP_per_capita']))
+      .range([0, width]);
+
+  svg.append('g')
+      .attr('transform', `translate(0,${height})`)
+      .call(d3.axisBottom(x));
 
   const y = d3.scaleLinear()
-    .domain(d3.extent(data, d => d[' gender_wage_gap'])).nice()
-    .range([height, 0]);
+      .domain(d3.extent(filteredData, d => +d[' gender_wage_gap']))
+      .range([height, 0]);
 
-  // Add X axis
-  svg.append("g")
-    .attr("class", "x-axis")
-    .attr("transform", `translate(0,${height})`)
-    .call(d3.axisBottom(x));
+  svg.append('g')
+      .call(d3.axisLeft(y));
 
-  // Add Y axis
-  svg.append("g")
-    .attr("class", "y-axis")
-    .call(d3.axisLeft(y));
+  const tooltip = d3.select('body').append('div')
+      .attr('class', 'tooltip')
+      .style('opacity', 0)
+      .style('position', 'absolute')
+      .style('background-color', 'white')
+      .style('border', 'solid')
+      .style('border-width', '1px')
+      .style('border-radius', '5px')
+      .style('padding', '10px');
 
-  // Add dots
-  svg.selectAll("dot")
-    .data(data)
-    .enter().append("circle")
-    .attr("r", 5)
-    .attr("cx", d => x(d[' GDP_per_capita']))
-    .attr("cy", d => y(d[' gender_wage_gap']))
-    .style("fill", "#69b3a2")
-    .style("stroke", "black")
-    .on("mouseover", function(event, d) {
-      tooltip.html(`
-        <strong>Country:</strong> ${d.Entity}<br/>
-        <strong>Year:</strong> ${d.Year}<br/>
-        <strong>Wage Gap:</strong> ${d[' gender_wage_gap']}<br/>
-        <strong>GDP per Capita:</strong> ${d[' GDP_per_capita']}
-      `)
-      .style("visibility", "visible");
-    })
-    .on("mousemove", function(event) {
-      tooltip.style("top", (event.pageY + 5) + "px")
-        .style("left", (event.pageX + 5) + "px");
-    })
-    .on("mouseout", function() {
-      tooltip.style("visibility", "hidden");
-    });
+  svg.append('g')
+      .selectAll('dot')
+      .data(filteredData)
+      .enter()
+      .append('circle')
+      .attr('cx', d => x(+d[' GDP_per_capita']))
+      .attr('cy', d => y(+d[' gender_wage_gap']))
+      .attr('r', 3)
+      .style('fill', '#69b3a2')
+      .on('mouseover', function(event, d) {
+          tooltip.transition()
+              .duration(200)
+              .style('opacity', .9);
+          tooltip.html(`Country: ${d.Entity}<br>GDP per capita: ${d[' GDP_per_capita']}<br>Wage gap: ${d[' gender_wage_gap']}`)
+              .style('left', (event.pageX + 5) + 'px')
+              .style('top', (event.pageY - 28) + 'px');
+      })
+      .on('mouseout', function() {
+          tooltip.transition()
+              .duration(500)
+              .style('opacity', 0);
+      });
 });
